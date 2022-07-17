8e25de9c89f701b566a7d71579a023dfd00abd45
puds09 <ph.pedro09@gmail.com>
2022-07-17 15:47:42 -0300

    questao_2 100% completed

--- Deque e Listas/questao_2.py
+++ Deque e Listas/questao_2.py
@@ -6,16 +6,12 @@ dq = deque()
 
 for char in entrada:
     if(char in ['*', '+']):
-        continue
+        if(char == '*'):
+            print(dq.popleft(), end="")
+        elif(char == '+'):
+            print(dq.pop(), end="")
+
     elif(char.isdigit()):
         dq.append(char)
     else:
         dq.appendleft(char) 
-
-
-for char in entrada:
-    if(char == '*'):
-        print(dq.popleft(), end="")
-    elif(char == '+'):
-        print(dq.pop(), end="")
-
