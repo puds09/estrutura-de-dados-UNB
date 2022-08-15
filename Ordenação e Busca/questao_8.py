c3f5fafe9a755f550c09ab9f1995eba8d11eba10
puds09 <ph.pedro09@gmail.com>
2022-08-15 11:42:28 -0300

    uploading questao_8

---
+++ Ordenação e Busca/questao_8.py
@@ -0,0 +1,33 @@
+
+def negative_modular():
+    current = number
+    module = k
+    while(True):
+        if(current + module > 0):
+            break
+        current += module
+
+    return current
+
+n, k = map(int, input().split())
+
+storage = []
+for i in range(n): 
+    number = int(input())
+    module = number % k if(number > 0) else negative_modular()
+    odd = number % 2
+    tuple = (module, -odd, -number if odd else number, number)
+    
+    storage.append(tuple)
+
+
+for element in sorted(storage, reverse=True): print(element[-1], end=" ")
+
+
+# # print(storage)
+# storage.sort(reverse=True)
+# # print(storage)
+
+# for i in range(len(storage)):
+#     print(storage[i][-1], end=" ")
+
