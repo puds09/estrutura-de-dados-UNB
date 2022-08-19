c974173a3116a593e84a654475b86e31e7dd52c3
puds09 <ph.pedro09@gmail.com>
2022-08-19 20:15:45 -0300

    questao_2 100% done

--- Árvores 2/questao_2.py
+++ Árvores 2/questao_2.py
@@ -1,11 +1,4 @@
 
-# class ArvoreBinaria():
-
-#     def __init__(self, dado, esq = None, dir = None):
-#         self.dado = dado
-#         self.esq = esq
-#         self.dir = dir
-
 def show(root, prefixo):
 
     if root is None: return
@@ -35,6 +28,7 @@ def nivel(root, data):
         if str(next).isnumeric(): 
             return next
 
+    return -1
 
 height = 0
 next = -1
@@ -45,8 +39,3 @@ def mostra_arvore_e_nivel(root, data):
     output = nivel(root, data)
     #just build the output
     print(f'nivel(raiz, {data}): {output}') if str(output).isnumeric() else print(f'nivel(raiz, {data}): -1')
-
-
-# root = ArvoreBinaria(7, ArvoreBinaria(4), ArvoreBinaria(10, ArvoreBinaria(11), ArvoreBinaria(15)))
-# data = 10
-# mostra_arvore_e_nivel(root, data)
