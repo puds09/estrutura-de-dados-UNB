fbada42e99b78e95822313b679d1e228c8c3fd9d
puds09 <ph.pedro09@gmail.com>
2022-08-24 19:58:41 -0300

    questao_3 Árvores-1 100% done

--- Árvores 1/questao_3.py
+++ Árvores 1/questao_3.py
@@ -10,7 +10,7 @@ def getElementById(raiz, id):
             if match: temp.append(element)
 
         elif id[-1] == "*":
-            match = re.findall(f'(.*{id[:-1]}.*)|(.*{id[:-1].upper()}.*)', element)
+            match = re.findall(f'(.*{id[:-1]}.*)|(.*{id[:-1].upper()}.*)|(.*{id[:-1].lower()}.*)', element)
             if match: temp.append(element)
 
         elif element == id :
@@ -21,7 +21,14 @@ def getElementById(raiz, id):
 
     return temp
 
-#caso resolvido:
+
+#casos resolvidos:
+# 1°
 # raiz = {'HTML': {'HEAD': {'TITLE': 'Título'},'BODY': {'H1': 'header', 'P': 'Lorem', 'Y': 'ipsum'}}}
 # print(sorted(getElementById(raiz, '*Y')))
- 	
+# Output: ['BODY', 'Y']
+
+# 2°
+# raiz = {'H1': {'H2': {'H3': {'H4': {'H5': {'h1' : 'header', 'h2': 'outro', 'h3': 'hello', 'h4': 'world'}}}}}, 'H2': {'H1': 'H1', 'H2': 'H2', 'H3': 'H3', 'H4': 'H4', 'H5': 'H5'}} 
+# print(sorted(getElementById(raiz, 'H*')))
+# Output: ['H1', 'H1', 'H2', 'H2', 'H2', 'H3', 'H3', 'H4', 'H4', 'H5', 'H5', 'h1', 'h2', 'h3', 'h4']
